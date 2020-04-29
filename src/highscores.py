e948d15df08d85c33848a6994b02f06cb30abe07
WojtAcht <wachtelik@gmail.com>
2020-04-29 23:47:03 +0200

    Added timer, fixed bug with color after import, fixed bug with multiple import windows, fixed hint bug when sudoku is solved.

--- src/highscores.py
+++ src/highscores.py
@@ -4,9 +4,10 @@ from tkinter import messagebox
 class HighScores:
     def __init__(self):
         self.scores_number = 10
-        self.scores = []
+        self.scores = self.read_scores()
 
     def add_score(self, score):
+        self.scores = self.read_scores()
         if len(self.scores) == self.scores_number:
             self.scores.append(score)
             self.scores.sort()
@@ -14,9 +15,26 @@ class HighScores:
         else:
             self.scores.append(score)
             self.scores.sort()
+        self.update_scores()
 
     def show_scores(self):
         string_result = "Your high scores: \n"
         for index, score in enumerate(self.scores):
             string_result = string_result + str(index + 1) + ". " + str(score) + " s \n"
         messagebox.showinfo("Scores: ", string_result)
+
+    def update_scores(self):
+        with open('score.txt', "w") as write_score:
+            for index, item in enumerate(self.scores):
+                if index == len(self.scores) - 1:
+                    write_score.write(str(item))
+                else:
+                    write_score.write(str(item) + ' ')
+        write_score.close
+
+    def read_scores(self):
+        with open('score.txt', "r") as read_score:
+            scores = read_score.read()
+            scores = scores.split(' ')
+            scores = list(map(float, scores))
+        return scores
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,6 @@
 from tkinter.font import Font
 from tkinter import *
+import datetime
 
 app = Tk()
 window_width = 1500
@@ -11,14 +12,18 @@ font_size = int(window_height / 25)
 normal_font = Font(family="Open Sans", size=font_size)
 bigger_font = Font(family="Open Sans", size=int(font_size * 3 / 2))
 
-
 def set_width(scale):
     return int(window_width * scale)
-
 
 def set_height(scale):
     return int(window_height * scale)
 
-
 def set_font(scale):
     return int(font_size * scale)
+
+start_time = datetime.datetime(100,1,1,23,59,59)
+
+def addSecs(tm, secs=1):
+    full_date = datetime.datetime(100, 1, 1, tm.hour, tm.minute, tm.second)
+    full_date = full_date + datetime.timedelta(seconds=secs)
+    return full_date.time()
